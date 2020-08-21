36669e4739c6cdaec56f625a3afbf248e4c458af
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-08-21 18:21:23 -0400

    Update peerlist.js

--- peerlist.js
+++ peerlist.js
@@ -8,7 +8,7 @@ var cleanObject = (ob) =>
     }
   }, {});
 
-async function getSearchResults(page){
+async function getSearchResults(page,sort){
   var res = await fetch("https://t1xpljqyri-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.32.0%3Breact-instantsearch%205.3.2%3BJS%20Helper%202.26.1&x-algolia-application-id=T1XPLJQYRI&x-algolia-api-key=aad3f891232f12e59187e9962e07493e", {
   "headers": {
     "accept": "application/json",
@@ -18,9 +18,9 @@ async function getSearchResults(page){
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "cross-site"
   },
-  "referrer": "https://www.peerlyst.com/search/users?trk=search_page_entity_types&query=%22Threat%20Intelligence%22&refinementList=%7B%22Expertise%22%3A%5B%22threat%20intelligence%22%5D%7D&page=1&sortBy=people_default",
+  "referrer": window.location.href,
   "referrerPolicy": "no-referrer-when-downgrade",
-  "body": `{\"requests\":[{\"indexName\":\"people_default\",\"params\":\"query=%22Threat%20Intelligence%22&hitsPerPage=200&maxValuesPerFacet=20&page=${page}&attributesToRetrieve=%5B%22*%22%5D&attributesToHighlight=%5B%5D&highlightPreTag=%3Cais-highlight-0000000000%3E&highlightPostTag=%3C%2Fais-highlight-0000000000%3E&facets=%5B%22Expertise%22%2C%22Certifications%22%2C%22Company%22%2C%22Industry%22%2C%22title%22%5D&tagFilters=&facetFilters=%5B%22Expertise%3Athreat%20intelligence%22%5D\"}]}`,
+  "body": `{\"requests\":[{\"indexName\":\"people_default\",\"params\":\"query=%22Threat%20Intelligence%22&hitsPerPage=2000&maxValuesPerFacet=20&page=${page}&attributesToRetrieve=%5B%22*%22%5D&attributesToHighlight=%5B%5D&highlightPreTag=%3Cais-highlight-0000000000%3E&highlightPostTag=%3C%2Fais-highlight-0000000000%3E&facets=%5B%22Expertise%22%2C%22Certifications%22%2C%22Company%22%2C%22Industry%22%2C%22title%22%5D&tagFilters=&facetFilters=%5B%22Expertise%3Athreat%20intelligence%22%5D\"}]}`,
   "method": "POST",
   "mode": "cors",
   "credentials": "omit"
@@ -50,7 +50,7 @@ function parseSearchResults(obj){
 
 async function loopThroughSearchResults(){
 
-  getSearchResults(4)
+  getSearchResults(0,'people_alphabetical')
 
 }
 
